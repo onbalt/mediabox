8f7ba02836d7b66bbb672ea60a6132b730bb71aa
Gino <ginocote@gmail.com>
2016-11-06 16:41:50 -0500

    New JS for CSS fade in fade out

--- src/mediabox.js
+++ src/mediabox.js
@@ -1,3 +1,9 @@
+var Newplayer = null,
+    plyrInstance = null,
+    playerMarkup;
+
+/*****************************************/
+
 (function (root, factory) {
     "use strict";
     if (typeof define === 'function' && define.amd) {
@@ -63,8 +69,9 @@
                 service.provider = "vimeo";
                 service.id       = matches[3];
             } else {
-                service.provider = "Unknown";
-                service.id       = '';
+                service.provider = "html5";
+                service.id       = url;
+                //service.id       = '12345';
             }
 
             return service;
@@ -78,21 +85,51 @@
             } else if (service.provider === 'vimeo') {
                 embedLink = 'https://player.vimeo.com/video/' + service.id;
             } else {
-                throw new Error("Invalid video URL");
+                embedLink = service.id ;
+                //throw new Error("Invalid video URL");
             }
-
+            
+            if (service.provider === 'youtube' || service.provider === 'vimeo' ) {
+                
             lightbox = this.template(
                 '<div class="mediabox-wrap" role="dialog" aria-hidden="false"><div class="mediabox-content" role="document" tabindex="0"><span class="mediabox-close" aria-label="close"></span><iframe src="{embed}?autoplay=1" frameborder="0" allowfullscreen></iframe></div></div>', {
                     embed: embedLink
                 });
 
+            } else {
+                
+                // LOADING GIF EXAMPLES
+                // http://rmhc.org.sg/wp-content/uploads/tvc/vidloading.gif
+                // https://s-media-cache-ak0.pinimg.com/originals/4f/43/2d/4f432d9234988a5f33b26e0ba06bc6fe.gif
+                // https://s18.postimg.org/gr6zd940p/loading2.gif
+                // https://s14.postimg.org/si566pj5d/image.gif
+                // https://s4.postimg.org/xklx83wnh/loading3.gif
+                
+                var ext = embedLink.split('.').pop();
+                ext = removeParam(ext);
+                if (!ext) ext = "mp4";
+                
+            lightbox = this.template(
+                '<div class="mediabox-wrap" role="dialog" aria-hidden="false"><div class="mediabox-content" role="document" tabindex="0"><span class="mediabox-close" aria-label="close"></span><video style="background-color:#000000" controls autoplay width="100%" height="100%" poster="https://s14.postimg.org/si566pj5d/image.gif"><source src="{embed}" type="video/'+ext+'">Your browser does not support the video tag.</video></div></div>', {
+                    embed: embedLink
+                });
+                
+            }
+                
+
             this.root.insertAdjacentHTML('beforeend', lightbox);
         },
         events: function () {
             var wrapper = document.querySelector('.mediabox-wrap');
-
+             
+            /* GC */
+            setTimeout(function(){  
+                wrapper.setAttribute("id", "mediabox-show");
+            }, 0);
+            
+            /*GC from close mediabox-content */
             wrapper.addEventListener('click', function (e) {
-                if (e.target && e.target.nodeName === 'SPAN' && e.target.className === 'mediabox-close' || e.target.nodeName === 'DIV' && e.target.className === 'mediabox-wrap') {
+                if (e.target && e.target.nodeName === 'SPAN' && e.target.className === 'mediabox-close' || e.target.nodeName === 'DIV' && e.target.className === 'mediabox-wrap' || e.target.nodeName === 'DIV' && e.target.className === 'mediabox-content') {
                     this.close(wrapper);
                 }
             }.bind(this), false);
@@ -105,7 +142,8 @@
                 clearTimeout(timer);
             }
 
-            el.classList.add('mediabox-hide');
+            //el.classList.add('mediabox-hide');
+            el.removeAttribute('id');
 
             timer = setTimeout(function() {
                 var el = document.querySelector('.mediabox-wrap');
@@ -118,3 +156,12 @@
 
     return MediaBox;
 }));
+
+
+    // Remove param
+    function removeParam(url) {
+        var value = url.substring(url.lastIndexOf('/') + 1);
+        //get the part after before ?
+        value = value.split("?")[0];
+        return value;
+    }
